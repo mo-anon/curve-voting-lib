d5b5ef64b2215c479c45b8b7e5e33d1375332a28
mo <moanonresearch@gmail.com>
2025-07-28 20:59:51 +0200

    fix

--- setup.py
+++ setup.py
@@ -8,10 +8,9 @@ setup(
     author_email="moanonresearch@gmail.com",
     packages=find_packages(),
     install_requires=[
-        "titanoboa==0.2.6",
+        "titanoboa>=0.2.6",
         "python-dotenv>=1.0.0",
         "hexbytes>=0.3.0",
-        "vyper>=0.4.1",
     ],
     python_requires=">=3.8",
 ) 
